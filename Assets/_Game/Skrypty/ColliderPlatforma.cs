471c230351b39519bcae75a35ebb31aaa95bc627
KacperSobolak <kacpersobolak@interia.eu>
2017-07-29 13:55:37 +0200

    Master

---
+++ Assets/_Game/Skrypty/ColliderPlatforma.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderPlatforma : MonoBehaviour
+{
+
+    GameObject GameController;
+    public bool dobry = true;
+
+    void Start()
+    {
+        GameController = GameObject.Find("GameController");
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Dodaje Punkt!");
+        if (dobry == true)
+        {
+            GameController gc = GameController.GetComponent<GameController>();
+            gc.punkty++;
+        }
+    }
+}
