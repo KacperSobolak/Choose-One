546be190f1def6ef430f396e0f57bcdcf3202e18
KacperSobolak <kacpersobolak@interia.eu>
2017-07-29 14:25:14 +0200

    master

--- Assets/_Game/Skrypty/GameController.cs
+++ Assets/_Game/Skrypty/GameController.cs
@@ -7,12 +7,26 @@ public class GameController : MonoBehaviour {
 
     public int punkty;
     public Text punktytext;
+    public GameObject pauzapanel;
 
 	void Start () {
-		
+        pauzapanel.SetActive(false);
 	}
 	
 	void Update () {
         punktytext.text = punkty.ToString();
 	}
+
+    public void PauzaBTN()
+    {
+        Time.timeScale = 0;
+        pauzapanel.SetActive(true);
+    }
+
+    public void UnpauseBTN()
+    {
+        Time.timeScale = 1;
+        pauzapanel.SetActive(false);
+    }
+
 }
